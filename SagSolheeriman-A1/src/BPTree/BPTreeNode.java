0c0b9415d7d0b2f0887bd5aefddbefbd593e53f8
AhmedSoliman7 <AhmedSoliman7@github.com>
2016-03-01 20:56:45 +0200

    Added insertAt method in InnerNode class

--- SagSolheeriman-A1/src/BPTree/BPTreeNode.java
+++ SagSolheeriman-A1/src/BPTree/BPTreeNode.java
@@ -1,9 +1,9 @@
 package BPTree;
 
-enum NodeType{
-	InnerNode,
-	LeafNode,
+enum NodeType {
+	InnerNode, LeafNode,
 }
+
 public abstract class BPTreeNode {
 
 	protected key[] keys;
@@ -11,28 +11,29 @@ public abstract class BPTreeNode {
 	protected BPTreeNode rightSibling;
 	protected BPTreeNode leftSibling;
 	protected int numberOfKeys;
-	protected int ORDER ;
-	public BPTreeNode(int n)
-	{
-		
+	protected int ORDER;
+
+	public BPTreeNode(int n) {
+
 		numberOfKeys = 0;
 		parentNode = null;
 		rightSibling = null;
 		leftSibling = null;
 		ORDER = n;
 	}
-	
-	public int countKeys(){
+
+	public int countKeys() {
 		return numberOfKeys;
 	}
-	
-	public boolean isRoot(){
+
+	public boolean isRoot() {
 		return (parentNode == null);
 	}
-	
-	public key getKey(int index){
+
+	public key getKey(int index) {
 		return (key) this.keys[index];
 	}
+
 	public void setKey(int index, key key) {
 		this.keys[index] = key;
 	}
@@ -43,31 +44,21 @@ public abstract class BPTreeNode {
 
 	public void setParent(BPTreeNode parent) {
 		this.parentNode = parent;
-	}	
-	
+	}
+
 	/**
-	 * for a leaf Node : Search a key on current node, if found the key then return its position,
-	 * otherwise return -1, 
-	 * for an Inner Node : return the child node index which should contain the key.
+	 * for a leaf Node : Search a key on current node, if found the key then
+	 * return its position, otherwise return -1, for an Inner Node : return the
+	 * child node index which should contain the key.
 	 */
 	public abstract int search(key key);
-	
-	
-	
+
 	public boolean isOverflow() {
 		return this.countKeys() == this.keys.length;
 	}
-	
+
 	public abstract NodeType getNodeType();
-	
+
 	protected abstract BPTreeNode split();
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
 }
